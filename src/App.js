34b08748d6b81f69ae728861a813c40ce90fda32
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-04 13:41:05 -0800

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -4,38 +4,62 @@ import Header from "./Components/Header/Header";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
-import { Provider } from "react-redux";
-import { store } from "./Redux/store";
 import ProductPage from "./Components/ProductPage/ProductPage";
 import Cart from "./Components/Cart/Cart";
 import NotFoundPage from "./Components/NotFound/NotFoundPage";
 import AuthPage from "./Components/AuthPage/AuthPage";
 import Payments from "./Components/Payments/Payments";
 import { AnimatePresence } from "framer-motion";
-function App() {
+import { connect } from "react-redux";
+import { check_User } from "./Redux/Actions/userActions";
+
+function App(props) {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  console.log("props.cart_Items: ", props.cart_Items.cart.cartItems);
   return (
     <div className="App">
-      <Provider store={store}>
-        <Header />
-        <AnimatePresence>
-          <Switch>
-            <Route path="/home/:Id" component={ProductPage} />
-            <Route exact path="/payments" component={Payments} />
-            <Route exact path="/auth" component={AuthPage} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/home" component={Home} />
-            <Route path="/not-found" component={NotFoundPage} />
-            <Redirect exact from="/" to="/home" />
-            <Redirect to="/not-found" />
-          </Switch>
-        </AnimatePresence>
-        <Footer />
-      </Provider>
+      <Header />
+      <AnimatePresence>
+        <Switch>
+          <Route path="/home/:Id" component={ProductPage} />
+          <Route exact path="/auth" component={AuthPage} />
+          <Route
+            exact
+            path="/payments"
+            component={
+              //if user is logout && cartItems are not equal to 0 then redirect to payment
+              //if user is login but cartItems === 0 then redirect to HomePage
+              //if user is logout redirect to AuthPage
+              props.user
+                ? props.cart_Items.cart.cartItems.length !== 0
+                  ? Payments
+                  : Home
+                : AuthPage
+            }
+          />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/home" component={Home} />
+          <Route path="/not-found" component={NotFoundPage} />
+          <Redirect exact from="/" to="/home" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </AnimatePresence>
+      <Footer />
     </div>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    cart_Items: state,
+    user: state.user,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    check_User: (user) => dispatch(check_User(user)),
+  };
+};
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
